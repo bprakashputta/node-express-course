6f0499159fc0cb804691e3bd131f9e7b57507907
Putta Bhanu Prakash <bhanu.putta1999@gmail.com>
2021-04-27 22:45:56 +0530

    Implemented login method

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
 const mockUserData = [
     {
@@ -12,6 +14,7 @@ const mockUserData = [
     }
 ]
 
+//Simple get method to get json response of user data
 app.get('/users', function (request, response){
     console.log(request.params.id)
     response.json({
@@ -21,6 +24,7 @@ app.get('/users', function (request, response){
     })
 })
 
+//Dynamic get route to pass variables from search bar
 app.get('/users/:id', function (request, response){
     console.log(request.params.id)
     response.json({
@@ -30,6 +34,31 @@ app.get('/users/:id', function (request, response){
     })
 })
 
-app.listen(8080, function (){
+//Writing a post request to implement simple login
+app.post('/login', function (request, response){
+    const username = request.body.username;
+    const password = request.body.password;
+
+    //mock username and passwords
+    const mockusername = "billyTheKid";
+    const mockpassword = "superSecret";
+
+    //check login condition
+    if(username == mockusername && password == mockpassword){
+        response.json({
+            success: true,
+            message: "password and email match!",
+            token: 'encrypted token goes here'
+        })
+    } else {
+        response.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+})
+
+//The server is listening on the port 8000
+app.listen(8000, function (){
     console.log("App is running on port 8080");
 })
