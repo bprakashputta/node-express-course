478c811e6dc572fdad576e1487bbf025831ca291
Putta Bhanu Prakash <bhanu.putta1999@gmail.com>
2021-04-27 21:55:18 +0530

    Added mock json userdata

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+    {
+        name: 'Mark',
+        age: '20'
+    },
+    {
+        name: 'Jill',
+        age: '21'
+    }
+]
+
+
+
 app.listen(8080, function (){
     console.log("App is running on port 8080");
 })
